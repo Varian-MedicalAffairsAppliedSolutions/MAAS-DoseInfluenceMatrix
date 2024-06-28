b3546b923e20d5a942ed7dbaa530ce286cd21a2c
hdp203 <142261976+hdp203@users.noreply.github.com>
2024-06-28 16:05:40 -0400

    Update DataClasses.cs

--- Source/DataClasses.cs
+++ Source/DataClasses.cs
@@ -23,31 +23,14 @@ namespace CalculateInfluenceMatrix
     public class DoseData
     {
         public DoseData() { }
-        public DoseData(List<DosePoint> points)
+        public DoseData(List<DosePoint> points, double dSumCutoffValues, int iNumCutoffValues)
         {
             dosePoints = points;
+            m_iNumCutoffValues = iNumCutoffValues;
+            m_dSumCutoffValues = dSumCutoffValues;
         }
         public List<DosePoint> dosePoints = new List<DosePoint>();
+        public double m_dSumCutoffValues;
+        public int m_iNumCutoffValues;
     };
-
-
-    public class TBeamMetaData
-    {
-        public string Id { get; set; } = "";
-        public string szEnergy { get; set; } = "";
-        public string szTechnique { get; set; } = "";
-        public string szMLCName { get; set; } = "";
-        public float fGantryRtn { get; set; } = 0;
-        public float fCollRtn { get; set; } = 0;
-        public float fJawX1 { get; set; } = 0;
-        public float fJawX2 { get; set; } = 0;
-        public float fJawY1 { get; set; } = 0;
-        public float fJawY2 { get; set; } = 0;
-        public float fPatientSuppAngle { get; set; } = 0;
-        public float fIsoX { get; set; } = 0;
-        public float fIsoY { get; set; } = 0;
-        public float fIsoZ { get; set; } = 0;
-        public float fSSD { get; set; } = 0;
-        public float fSAD { get; set; } = 100;
-    }
 }
