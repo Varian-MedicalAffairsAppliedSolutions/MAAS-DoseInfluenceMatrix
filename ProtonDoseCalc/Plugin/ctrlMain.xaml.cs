04b9d47b9db3a95705578923e20eb98cd43a4b3c
japnp <japankpatel@hotmail.com>
2025-04-04 17:31:29 +0000

    prelim interface revision - added license and warning

--- ProtonDoseCalc/Plugin/ctrlMain.xaml.cs
+++ ProtonDoseCalc/Plugin/ctrlMain.xaml.cs
@@ -1,7 +1,72 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.Remoting.Contexts;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+//using System.Windows.Threading;
+//using VMS.TPS.Common.Model.API;
+
+//namespace CalculateInfluenceMatrix
+//{
+//    /// <summary>
+//    /// Interaction logic for UserControl1.xaml
+//    /// </summary>
+//    public partial class ctrlMain : UserControl
+//    {
+//        VMS.TPS.Script m_hScript;
+//        System.Windows.Window m_hMainWnd;
+
+//        public ctrlMain()
+//        {
+//            InitializeComponent();
+//        }
+//        public ctrlMain(VMS.TPS.Script script, System.Windows.Window hMainWnd)
+//        {
+//            m_hScript = script;
+//            InitializeComponent();
+//            m_hMainWnd = hMainWnd;
+//        }
+
+//        private void butCalculate_Click(object sender, RoutedEventArgs e)
+//        {
+//            butClose.IsEnabled = false;
+//            butCalculate.IsEnabled = false;
+
+//            m_hScript.RunInfMatrixCalc();
+
+//            butClose.IsEnabled = true;
+//            butCalculate.IsEnabled = true;
+//        }
+//        public void AddMessage(string szMsg)
+//        {
+//            txtMessages.Text = txtMessages.Text + "\n" + szMsg;
+//            txtMessages.ScrollToEnd();
+//            Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.ContextIdle, null);
+//        }
+
+//        private void butClose_Click(object sender, RoutedEventArgs e)
+//        {
+//            m_hMainWnd.Close();
+//        }
+//    }
+//}
+
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,20 +86,116 @@ namespace CalculateInfluenceMatrix
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class ctrlMain : UserControl
+    public partial class ctrlMain : UserControl, INotifyPropertyChanged
     {
         VMS.TPS.Script m_hScript;
         System.Windows.Window m_hMainWnd;
 
+        private string _validationWarning = string.Empty;
+        public string ValidationWarning
+        {
+            get { return _validationWarning; }
+            set
+            {
+                _validationWarning = value;
+                OnPropertyChanged("ValidationWarning");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
         public ctrlMain()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
+
         public ctrlMain(VMS.TPS.Script script, System.Windows.Window hMainWnd)
         {
             m_hScript = script;
-            InitializeComponent();
             m_hMainWnd = hMainWnd;
+
+            InitializeComponent();
+            this.DataContext = this;
+
+            // Check for license expiration (NOEXPIRE flag)
+            CheckLicenseExpiration();
+
+            // Check validation status
+            CheckValidationStatus();
+        }
+
+        private void CheckLicenseExpiration()
+        {
+            // Check for NOEXPIRE environment variable
+            if (Environment.GetEnvironmentVariable("NOEXPIRE") == null)
+            {
+                // Handle license expiration logic here
+                // For example, check license file, etc.
+            }
+        }
+
+        private void CheckValidationStatus()
+        {
+            try
+            {
+                // Path to config file - adjust as needed
+                string configPath = System.IO.Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+                    "YourCompany", "DoseInfluenceMatrix", "config.ini");
+
+                bool validated = false;
+
+                if (System.IO.File.Exists(configPath))
+                {
+                    // Read config file
+                    string[] lines = System.IO.File.ReadAllLines(configPath);
+                    foreach (string line in lines)
+                    {
+                        if (line.StartsWith("Validated="))
+                        {
+                            validated = line.Equals("Validated=True", StringComparison.OrdinalIgnoreCase);
+                            break;
+                        }
+                    }
+                }
+
+                // Set validation warning if not validated
+                if (!validated)
+                {
+                    ValidationWarning = "***Not validated for Clinical Use***";
+                }
+            }
+            catch (Exception)
+            {
+                // If there's an error reading config, default to not validated
+                ValidationWarning = "***Not validated for Clinical Use***";
+            }
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = e.Uri.AbsoluteUri,
+                    UseShellExecute = true
+                });
+            }
+            catch
+            {
+                MessageBox.Show("Could not open license URL.");
+            }
+            e.Handled = true;
         }
 
         private void butCalculate_Click(object sender, RoutedEventArgs e)
@@ -47,11 +208,20 @@ namespace CalculateInfluenceMatrix
             butClose.IsEnabled = true;
             butCalculate.IsEnabled = true;
         }
+
         public void AddMessage(string szMsg)
         {
             txtMessages.Text = txtMessages.Text + "\n" + szMsg;
             txtMessages.ScrollToEnd();
-            Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.ContextIdle, null);
+
+            try
+            {
+                this.Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.ContextIdle, null);
+            }
+            catch
+            {
+                // Ignore dispatcher errors
+            }
         }
 
         private void butClose_Click(object sender, RoutedEventArgs e)
