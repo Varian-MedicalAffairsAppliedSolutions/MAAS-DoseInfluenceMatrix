d708ff09714f7a04be831497c81aae446a343b0a
japnp <japankpatel@hotmail.com>
2025-04-05 01:42:23 +0000

    validation message connected to config - photon

--- PhotonDoseCalc/Plugin/ctrlMain.xaml.cs
+++ PhotonDoseCalc/Plugin/ctrlMain.xaml.cs
@@ -1,39 +1,113 @@
 ﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Runtime.Remoting.Contexts;
-//using System.Text;
-//using System.Threading.Tasks;
+//using System.ComponentModel;
+//using System.Diagnostics;
 //using System.Windows;
 //using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
 //using System.Windows.Navigation;
-//using System.Windows.Shapes;
 //using System.Windows.Threading;
 
 //namespace PhotonInfluenceMatrixCalc
 //{
 //    /// <summary>
-//    /// Interaction logic for UserControl1.xaml
+//    /// Interaction logic for ctrlMain.xaml
 //    /// </summary>
-//    public partial class ctrlMain : UserControl
+//    public partial class ctrlMain : UserControl, INotifyPropertyChanged
 //    {
 //        VMS.TPS.Script m_hScript;
 //        System.Windows.Window m_hMainWnd;
 
+//        private string _validationWarning = string.Empty;
+//        public string ValidationWarning
+//        {
+//            get { return _validationWarning; }
+//            set
+//            {
+//                _validationWarning = value;
+//                OnPropertyChanged("ValidationWarning");
+//            }
+//        }
+
+//        public event PropertyChangedEventHandler PropertyChanged;
+//        protected void OnPropertyChanged(string name)
+//        {
+//            PropertyChangedEventHandler handler = PropertyChanged;
+//            if (handler != null)
+//            {
+//                handler(this, new PropertyChangedEventArgs(name));
+//            }
+//        }
+
 //        public ctrlMain()
 //        {
 //            InitializeComponent();
+//            this.DataContext = this;
 //        }
+
 //        public ctrlMain(VMS.TPS.Script script, System.Windows.Window hMainWnd)
 //        {
 //            m_hScript = script;
-//            InitializeComponent();
 //            m_hMainWnd = hMainWnd;
+
+//            InitializeComponent();
+//            this.DataContext = this;
+
+//            // Check validation status
+//            CheckValidationStatus();
+//        }
+
+//        private void CheckValidationStatus()
+//        {
+//            try
+//            {
+//                // Path to config file - adjust as needed
+//                string configPath = System.IO.Path.Combine(
+//                    Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+//                    "YourCompany", "PhotonInfluenceMatrix", "config.ini");
+
+//                bool validated = false;
+
+//                if (System.IO.File.Exists(configPath))
+//                {
+//                    // Read config file
+//                    string[] lines = System.IO.File.ReadAllLines(configPath);
+//                    foreach (string line in lines)
+//                    {
+//                        if (line.StartsWith("Validated="))
+//                        {
+//                            validated = line.Equals("Validated=True", StringComparison.OrdinalIgnoreCase);
+//                            break;
+//                        }
+//                    }
+//                }
+
+//                // Set validation warning if not validated
+//                if (!validated)
+//                {
+//                    ValidationWarning = "***Not validated for Clinical Use***";
+//                }
+//            }
+//            catch (Exception)
+//            {
+//                // If there's an error reading config, default to not validated
+//                ValidationWarning = "***Not validated for Clinical Use***";
+//            }
+//        }
+
+//        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+//        {
+//            try
+//            {
+//                Process.Start(new ProcessStartInfo
+//                {
+//                    FileName = e.Uri.AbsoluteUri,
+//                    UseShellExecute = true
+//                });
+//            }
+//            catch
+//            {
+//                MessageBox.Show("Could not open license URL.");
+//            }
+//            e.Handled = true;
 //        }
 
 //        private void butCalculate_Click(object sender, RoutedEventArgs e)
@@ -46,11 +120,20 @@
 //            butClose.IsEnabled = true;
 //            butCalculate.IsEnabled = true;
 //        }
+
 //        public void AddMessage(string szMsg)
 //        {
 //            txtMessages.Text = txtMessages.Text + "\n" + szMsg;
 //            txtMessages.ScrollToEnd();
-//            Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.ContextIdle, null);
+
+//            try
+//            {
+//                this.Dispatcher.Invoke(new Action(() => { }), DispatcherPriority.ContextIdle, null);
+//            }
+//            catch
+//            {
+//                // Ignore dispatcher errors
+//            }
 //        }
 
 //        private void butClose_Click(object sender, RoutedEventArgs e)
@@ -60,14 +143,17 @@
 //    }
 //}
 
-
 using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using System.Windows.Threading;
+using System.Xml.Linq;
 
 namespace PhotonInfluenceMatrixCalc
 {
@@ -104,6 +190,9 @@ namespace PhotonInfluenceMatrixCalc
         {
             InitializeComponent();
             this.DataContext = this;
+
+            // Check validation status
+            CheckValidationStatus();
         }
 
         public ctrlMain(VMS.TPS.Script script, System.Windows.Window hMainWnd)
@@ -122,36 +211,41 @@ namespace PhotonInfluenceMatrixCalc
         {
             try
             {
-                // Path to config file - adjust as needed
-                string configPath = System.IO.Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
-                    "YourCompany", "PhotonInfluenceMatrix", "config.ini");
+                // Get the location of the executing assembly
+                string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+                string configPath = assemblyLocation + ".config";
 
-                bool validated = false;
+                // Default to showing the warning
+                ValidationWarning = "***Not validated for Clinical Use***";
 
-                if (System.IO.File.Exists(configPath))
+                if (File.Exists(configPath))
                 {
-                    // Read config file
-                    string[] lines = System.IO.File.ReadAllLines(configPath);
-                    foreach (string line in lines)
+                    var doc = XDocument.Load(configPath);
+                    var validationSetting = doc
+                        .Descendants("configuration")
+                        .Descendants("appSettings")
+                        .Descendants("add")
+                        .Where(x => x.Attribute("key")?.Value == "Validation")
+                        .Select(x => x.Attribute("value")?.Value)
+                        .FirstOrDefault();
+
+                    // If validation is explicitly set to "true", clear the warning
+                    if (!string.IsNullOrEmpty(validationSetting) &&
+                        validationSetting.Equals("true", StringComparison.OrdinalIgnoreCase))
                     {
-                        if (line.StartsWith("Validated="))
-                        {
-                            validated = line.Equals("Validated=True", StringComparison.OrdinalIgnoreCase);
-                            break;
-                        }
+                        ValidationWarning = string.Empty;
                     }
-                }
 
-                // Set validation warning if not validated
-                if (!validated)
-                {
-                    ValidationWarning = "***Not validated for Clinical Use***";
+                    // Optional debugging
+                    // MessageBox.Show($"Config path: {configPath}\nValidation setting: {validationSetting}");
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                // If there's an error reading config, default to not validated
+                // Log error if needed
+                // MessageBox.Show($"Error checking validation: {ex.Message}");
+
+                // Default to showing the warning on error
                 ValidationWarning = "***Not validated for Clinical Use***";
             }
         }
